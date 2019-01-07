4d6dc9744a80c1e44affd85bf213bf532d88fbbb
umezawa.kanta <kanta0930@gmail.com>
2019-01-08 06:39:19 +0900

    first commit to cs

---
+++ rectangle.cs
@@ -0,0 +1,11 @@
+using System;
+public class Program
+{
+    public static void Main()
+    {
+    int width=5;
+    int height=10;
+    int area=width*height;
+    Console.WriteLine("Area of rectangle="+area);
+    }
+}
