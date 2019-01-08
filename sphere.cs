c172df464a88ec93d2e00aa6cca55f34b9eee7a1
umezawa.kanta <kanta0930@gmail.com>
2019-01-08 13:29:49 +0900

    add volumeOfSphere

---
+++ sphere.cs
@@ -0,0 +1,12 @@
+using System;
+public class Program
+{
+    public static void Main()
+    {
+    int radius=48;
+    double pie=3.142857142856;
+    double volume=(4.0/3.0)*pie*(radius*radius*radius);
+
+    Console.WriteLine("Volume of sphere="+volume);
+    }
+}
